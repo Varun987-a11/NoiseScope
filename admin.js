0f05bcae58fcb51b74523602eced6d4cafd20229
Varun kumar S <188704583+Varun987-a11@users.noreply.github.com>
2026-02-11 23:25:45 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -6,7 +6,7 @@ let noiseChart;
 // --- Map Initialization & Visualization ---
 function initializeMap() {
     map = L.map('map').setView([28.6139, 77.2090], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {  
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 }
